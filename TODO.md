aeb7761ba6db6d2718b6cedc0f6a6542b8355884
paulhayes <paul.hayes@entropedia.co.uk>
2013-05-05 16:06:02 +0100

    created todo file

---
+++ TODO.md
@@ -0,0 +1,12 @@
+TODO
+====
+
+
+Sensory events
+______________
+
+sounds emiited from an item, are transmitted to all other items in the room that have an onHear or onSee method.
+
+Doors that receive sound, can pass the sound onto their sibling door, and from there into the adjacent room. Door which are open, pass not just sound but also vision into the adjancent room.
+
+A sound recorcing instrucment could catch all transmitted sounds, and store them by time into a data object. Which then when played back, and listend to by the player, could be heard and repeated.
--- main.js
+++ main.js
@@ -18,13 +18,3 @@ player.see('You slowly open your eyes. Your in a dark cramped bunk.');
 
 engine.Shell.attachPlayerToShell( player );
 
-
-/*
-
-sounds emiited from an item, are transmitted to all other items in the room that have an onHear or onSee method.
-
-Doors that receive sound, can pass the sound onto their sibling door, and from there into the adjacent room. Door which are open, pass not just sound but also vision into the adjancent room.
-
-A sound recorcing instrucment could catch all transmitted sounds, and store them by time into a data object. Which then when played back, and listend to by the player, could be heard and repeated.
-
-*/
