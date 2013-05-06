a5182c521e46af8768bdb520c273559251ebc706
paulhayes <paul.hayes@entropedia.co.uk>
2013-05-06 04:35:52 +0100

    Created npc's, allowed for their person descriptions of scenens and player acitons.

--- TODO.md
+++ TODO.md
@@ -9,3 +9,7 @@ sounds emiited from an item, are transmitted to all other items in the room that
 Doors that receive sound, can pass the sound onto their sibling door, and from there into the adjacent room. Door which are open, pass not just sound but also vision into the adjancent room.
 
 A sound recorcing instrucment could catch all transmitted sounds, and store them by time into a data object. Which then when played back, and listend to by the player, could be heard and repeated.
+
+Dialog system
+_____________
+Players can converse to nps, using a dialog tree.
--- main.js
+++ main.js
@@ -1,7 +1,37 @@
 var engine = require('text_based_adventure_engine');
-var player = new engine.Player();
+var player = new engine.Player({ name : 'Bernard', fullname : 'Bernard Manning', subjectPronoun : 'he', objectPronoun : 'him' });
+
+/* Switch to past tense third person */
+
+Array.prototype.niceJoin.prefix = 'There was ';
+engine.Room.prototype.lookAroundPrefix =  '%(player.name)s looked around, %(player.subjectPronoun)s was in %(object.description)s. ';
+
+/* The player starts in a Train Carraige */
+var trainCompartment = new engine.Room({ name : 'train compartment', description : 'a 2nd class train compartment' });
+trainCompartment.addItem( player );
+
+/* Arrvial at different stations at fixed times */
+
+/* There are two other characters in the compartment. An old gentleman and a young lady. */
+
+var gentleman = new engine.NPC({ name : 'Elijah', fullname : 'Elijah Altt Hewitson', subjectPronoun : 'he', description : 'a little old man on the seat opposite %(player.objectPronoun)s' });
+trainCompartment.addItem( gentleman );
+
+var lady = new engine.NPC({ name : 'Audrey', fullname : 'Audrey W. Haugh', subjectPronoun : 'she', description : 'a young lady next to the window' });
+trainCompartment.addItem( lady );
 
 
+/* The gentleman has a pamphet which he'll give you if you complete the correct conversation path */
+
+/* If you hit a particular conversation path with the woman, she'll leave the carriage */
+
+/* Third character a ticket incepect arrives to check tickets */
+
+/* Player has a briefcase which containers Sandwiches, and an apple. You of course also have a ticket */
+
+/*
+
+Previous example 
 
 startRoom = new engine.Room({ name : 'bunk room', description : 'a cramped little room, the walls appear to be a dark and metallic, there\'s a dim dripping sound in the distance' });
 startRoom.addItem( new engine.Dice(), new engine.Clock(), new engine.Television() );
@@ -16,5 +46,6 @@ startRoom.addItem( player );
 
 player.see('You slowly open your eyes. Your in a dark cramped bunk.');
 
-engine.Shell.attachPlayerToShell( player );
+*/
 
+engine.Shell.attachPlayerToShell( player );
